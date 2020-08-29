b9e17a514f91ab55aafea29366228ebd57268a55
sathyavathi.ananda <sathyavathi.ananda@accenture.com>
2020-08-29 20:56:17 +0530

    updated magazin file

--- spa-without-framework-master 2/src/index.js
+++ spa-without-framework-master 2/src/index.js
@@ -5,12 +5,17 @@ import {Router,Routes} from './libs/router.js'
     const routeConfig = [
         new Routes({
         path:'home',
-        url: './views/src/views/detail.html'
+        url: './views/src/views/home.html'
     }),
     new Routes({
         path:'shop',
-        url: './views/src/views/landing.html'
-    },true)]
+        url: './views/src/views/shop.html'
+    },true),
+    new Routes({
+        path:'magazine',
+        url: './views/src/views/magazine.html'
+    },true),
+    ]
     
     new Router(routeConfig,'app');
 })()
